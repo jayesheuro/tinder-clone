2e077f7b43870936d4239ea918dcf7cf644ca253
Jayesh Singh <jayeshsingh001@gmail.com>
2021-03-24 09:50:53 +0530

    styled-header-component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build the MERN tinder clone</h1>
+      <Header />
     </div>
   );
 }
